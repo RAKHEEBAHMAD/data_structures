a806a463e4a9e29c39d71845acd47a36bb4c6610
rakheeb <rakheeb1905@gmail.com>
2023-10-10 22:05:28 +0530

    rotten oranges

---
+++ rotten_oranges.cpp
@@ -0,0 +1,61 @@
+class Solution 
+{
+    public:
+    //Function to find minimum time required to rot all oranges. 
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    q.push({{i,j},0});
+                }
+            }
+        }
+        
+        int res=0;
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+        while(!q.empty())
+        {
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int t=q.front().second;
+            
+            q.pop();
+            
+            res=max(res,t);
+            
+            for(int i=0;i<4;i++)
+            {
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and !vis[nrow][ncol] and grid[nrow][ncol]==1)
+                {
+                    grid[nrow][ncol]=2;
+                    q.push({{nrow,ncol},t+1});
+                    vis[nrow][ncol]=1;
+                }
+            }
+            
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    return -1;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
